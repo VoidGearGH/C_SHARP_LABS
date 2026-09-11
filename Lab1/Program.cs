976d8bbabf0c2f96231e71101fd9c133c40e992e
VoidGearGH <voidgear538@gmail.com>
2026-09-11 15:49:16 +0300

    Initial commit

---
+++ Lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Labs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? sequencesPath = Console.ReadLine();
+            string sequencesLines = 
+        }
+    }
+}
