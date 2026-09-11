7db67bd4dee77057fdcba265cbefc285a382abb3
VoidGearGH <voidgear538@gmail.com>
2026-09-11 15:59:50 +0300

    Added reading the input files

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,8 +6,23 @@ namespace Labs
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input the sequenceses path\n");
             string? sequencesPath = Console.ReadLine();
-            string sequencesLines = 
+            string[]? sequencesLines = File.Exists(sequencesPath) ? File.ReadAllLines(sequencesPath) : null;
+            
+            if(sequencesLines == null || !File.Exists(sequencesPath))
+            {
+                Console.WriteLine($"File which is by path {sequencesPath} is empty, or the path is wrong!");
+            }
+            
+            Console.WriteLine("Input the sequenceses path\n");
+            string? commandsPath = Console.ReadLine();
+            string[]? commandsLines = File.Exists(commandsPath) ? File.ReadAllLines(commandsPath) : null;
+            
+            if(commandsLines == null || !File.Exists(commandsPath))
+            {
+                Console.WriteLine($"File which is by path {commandsPath} is empty, or the path is wrong!");
+            }
         }
     }
 }
