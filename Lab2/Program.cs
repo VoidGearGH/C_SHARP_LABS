2357252ec017ab224046bdb9fd9189338addb165
VoidGearGH <voidgear538@gmail.com>
2026-09-18 15:55:24 +0300

    deleted the excess method

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -11,32 +11,6 @@ namespace Lab2
         Playing,
         NotInGame
     }
-
-    class IntValidator
-    {
-        private static bool IsDigitsOnly(string str)
-        {
-            if (string.IsNullOrEmpty(str))
-            {
-                return false;
-            }
-
-            foreach (char c in str)
-            {
-                if (c < '0' || c > '9')
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        public static bool Validate(string input)
-        {
-            return IsDigitsOnly(input);
-        }
-    }
-
     class Player
     {
         public string Name { get; private set; }
