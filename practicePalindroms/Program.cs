e31329636d237caaa5a6fc36b66f8c088622376b
VoidGearGH <voidgear538@gmail.com>
2026-09-18 15:20:53 +0300

    Added the practice of the lesson
    Is palindrom?

---
+++ practicePalindroms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace practicePalindroms
+{
+    internal class Program
+    {
+        static bool isPalindrom(string? s)
+        {
+            if (s == null) return true;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                if(Char.IsLetterOrDigit(c)) sb.Append(c);
+            }
+
+            for (int i = 0, j = sb.Length - 1; i < j; ++i, --j)
+            {
+                if (sb[i] != sb[j]) return false;
+            }
+
+            return true;
+        }
+        static void Main()
+        {
+            string? s = Console.ReadLine();
+            Console.WriteLine(isPalindrom(s));
+        }
+    }
+}
