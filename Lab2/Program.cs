ace6c166722aee73f05a84499663d3e313197cc5
VoidGearGH <voidgear538@gmail.com>
2026-09-18 13:53:35 +0300

    Fixed some logics

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,72 +1,62 @@
 ﻿using System;
-using System.Text;
 
 namespace Lab2
 {
     enum State
     {
         Winner,
-        Looser,
+        Loser,
         Playing,
         NotInGame
     }
+
     class IntValidator
     {
-        private static void CheckAsc(string? str, ref bool validateFlag)
+        private static bool IsDigitsOnly(string? str)
         {
-            if (str == null)
+            if (string.IsNullOrEmpty(str))
             {
-                Console.WriteLine("String without container!");
-                Environment.Exit(0);
+                return false;
             }
 
             foreach (char c in str)
             {
-                int ascNum = (int)c;
-                if (!(ascNum >= 49 && ascNum <= 58))
+                if (c < '0' || c > '9')
                 {
-                    validateFlag = true;
-                    break;
+                    return false;
                 }
             }
+            return true;
         }
-        public static void Validate(int value, string name)
+        public static bool Validate(string? input)
         {
-            string valueString = Convert.ToString(value);
-            bool validateFlag = false;
-
-            CheckAsc(valueString, ref validateFlag);
-
-            if(validateFlag)
-            {
-                validateFlag = false;
-                do
-                {
-                    string? newValueString = Console.ReadLine();
-                    CheckAsc(newValueString, ref validateFlag);
-                }
-                while (validateFlag);
-            }
+            return IsDigitsOnly(input);
         }
     }
+
     class Player
     {
-        public string Name {  get; private set; }
+        public string Name { get; private set; }
         public int Location { get; private set; } = -1;
         public State State { get; private set; } = State.NotInGame;
-
         public int DistanceTravelled { get; private set; } = 0;
-        public Player(string name) 
+
+        public Player(string name)
         {
             Name = name;
         }
+
         public void Move(int steps)
         {
 
         }
     }
+
     internal class Program
     {
-                
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
